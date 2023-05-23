6d019c6b11dd145dff8feb1a5ccc07c10797bdd4
erhanbasaran <erha0045@stud.kea.dk>
2023-05-23 21:01:26 +0200

    RabbitMQ publisher changed

--- RabbitMQ/RabbitMQProducer.cs
+++ RabbitMQ/RabbitMQProducer.cs
@@ -1,35 +1,11 @@
 using RabbitMQ.Client;
 using System.Text.Json;
 using System.Text;
+using NyWine.RabbitMQ;
 
 namespace NyWine.RabbitMQ
 {
     public class RabbitMQProducer : IMessageProducer
-    {
-        public void PublishMessage<T>(T message)
-        {
-            var factory = new ConnectionFactory()
-            {
-                HostName = "localhost",
-                UserName = "guest",
-                Password = "guest",
-                VirtualHost = "/"
-            };
-
-            var conn = factory.CreateConnection();
-
-            using var channel = conn.CreateModel();
-
-            channel.QueueDeclare("wines", durable: true, exclusive: false);
-
-            var jsonString = JsonSerializer.Serialize(message);
-            var body = Encoding.UTF8.GetBytes(jsonString);
-
-            channel.BasicPublish("", "wines", body: body);
-        }
-    }
-
-    /* public class RabbitMQProducer
     {
         private readonly RabbitMQConfiguration _rabbitMQConfig;
 
@@ -38,34 +14,27 @@ namespace NyWine.RabbitMQ
             _rabbitMQConfig = rabbitMQConfig;
         }
 
-        public void PublishMessage<T>(T message, string routingKey)
-    where T : class
+        public void PublishMessage<T>(T message)
         {
-            var factory = new ConnectionFactory
+            var factory = new ConnectionFactory()
             {
+
                 HostName = _rabbitMQConfig.Hostname,
                 UserName = _rabbitMQConfig.UserName,
-                Password = _rabbitMQConfig.Password
+                Password = _rabbitMQConfig.Password,
+                //VirtualHost = "/"
             };
 
-            using (var connection = factory.CreateConnection())
-            {
-                using (var channel = connection.CreateModel())
-                {
-                    channel.ExchangeDeclare(_rabbitMQConfig.ExchangeName, ExchangeType.Topic, durable: true);
+            var conn = factory.CreateConnection();
 
-                    var json = JsonSerializer.Serialize(message);
-                    var body = Encoding.UTF8.GetBytes(json);
+            using var channel = conn.CreateModel();
 
-                    channel.BasicPublish(
-                        exchange: _rabbitMQConfig.ExchangeName,
-                        routingKey: routingKey,
-                        basicProperties: null,
-                        body: body
-                    );
-                }
-            }
+            channel.QueueDeclare("wineQueue", durable: true, exclusive: false);
+
+            var jsonString = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(jsonString);
+
+            channel.BasicPublish("", "wineQueue", body: body);
         }
-    } */
-
+    }
 }
